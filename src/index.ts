d3bc2aac3cff8849f243a7bc63223ac9f5715a5f
k3mystra <programmer.coleslaw981@slmail.me>
2025-08-04 16:02:17 +0800

    refactor: Move middleware into its section

--- src/index.ts
+++ src/index.ts
@@ -8,7 +8,8 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 3111;
 
-// Middleware
+
+// -- Middlewares --
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
@@ -18,23 +19,6 @@ app.use((req: Request, res: Response, next: NextFunction) => {
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
   next();
-});
-
-// Health check
-app.get('/health', (req: Request, res: Response) => {
-  res.status(200).json({ status: 'OK', timestamp: new Date().toISOString() });
-});
-
-// Root endpoint
-app.get('/', (req: Request, res: Response) => {
-  res.json({ message: 'Welcome to the Express.js TypeScript API!' });
-});
-
-// Get the ZK proof from PROVER_URL
-app.post('/api/generate-zkp', (req: Request, res: Response) => {
-    axios.post(process.env.PROVER_URL || "", req.body, { headers: { 'Content-Type': 'application/json' } })
-        .then(prover_response => res.json(prover_response.data))
-        .catch(err => console.error(err));
 });
 
 // Error handling middleware
@@ -54,6 +38,25 @@ app.use(
 // 404 handler
 app.use('/{*any}', (req: Request, res: Response) => {
   res.status(404).json({ error: 'Route not found' });
+});
+
+
+// -- Endpoints --
+// Health check
+app.get('/health', (req: Request, res: Response) => {
+  res.status(200).json({ status: 'OK', timestamp: new Date().toISOString() });
+});
+
+// Root endpoint
+app.get('/', (req: Request, res: Response) => {
+  res.json({ message: 'Welcome to the Express.js TypeScript API!' });
+});
+
+// Get the ZK proof from PROVER_URL
+app.post('/api/generate-zkp', (req: Request, res: Response) => {
+    axios.post(process.env.PROVER_URL || "", req.body, { headers: { 'Content-Type': 'application/json' } })
+        .then(prover_response => res.json(prover_response.data))
+        .catch(err => console.error(err));
 });
 
 // Start server
